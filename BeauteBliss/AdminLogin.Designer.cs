3537d66cdcbc35ac496b037c02e98024c6211b91
wan nursakinah <sakinahzahrah166@gmail.com>
2026-06-22 02:47:22 +0800

    admin management module

---
+++ BeauteBliss/AdminLogin.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace BeauteBliss
+{
+    partial class AdminLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminLogin));
+            this.lbllogin = new System.Windows.Forms.Label();
+            this.lblusername = new System.Windows.Forms.Label();
+            this.lblpassword = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.tbusername = new System.Windows.Forms.TextBox();
+            this.tbpassword = new System.Windows.Forms.TextBox();
+            this.cbpassword = new System.Windows.Forms.CheckBox();
+            this.pbadmin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbadmin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbllogin
+            // 
+            this.lbllogin.AutoSize = true;
+            this.lbllogin.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(85)))), ((int)(((byte)(116)))));
+            this.lbllogin.Location = new System.Drawing.Point(551, 91);
+            this.lbllogin.Name = "lbllogin";
+            this.lbllogin.Size = new System.Drawing.Size(209, 38);
+            this.lbllogin.TabIndex = 0;
+            this.lbllogin.Text = "ADMIN LOGIN";
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Location = new System.Drawing.Point(441, 198);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(70, 16);
+            this.lblusername.TabIndex = 1;
+            this.lblusername.Text = "Username";
+            // 
+            // lblpassword
+            // 
+            this.lblpassword.AutoSize = true;
+            this.lblpassword.Location = new System.Drawing.Point(441, 251);
+            this.lblpassword.Name = "lblpassword";
+            this.lblpassword.Size = new System.Drawing.Size(67, 16);
+            this.lblpassword.TabIndex = 2;
+            this.lblpassword.Text = "Password";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(85)))), ((int)(((byte)(116)))));
+            this.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlogin.ForeColor = System.Drawing.Color.White;
+            this.btnlogin.Location = new System.Drawing.Point(528, 366);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(251, 45);
+            this.btnlogin.TabIndex = 3;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // tbusername
+            // 
+            this.tbusername.Location = new System.Drawing.Point(519, 196);
+            this.tbusername.Name = "tbusername";
+            this.tbusername.Size = new System.Drawing.Size(258, 22);
+            this.tbusername.TabIndex = 4;
+            // 
+            // tbpassword
+            // 
+            this.tbpassword.Location = new System.Drawing.Point(519, 248);
+            this.tbpassword.Name = "tbpassword";
+            this.tbpassword.Size = new System.Drawing.Size(258, 22);
+            this.tbpassword.TabIndex = 5;
+            this.tbpassword.UseSystemPasswordChar = true;
+            // 
+            // cbpassword
+            // 
+            this.cbpassword.AutoSize = true;
+            this.cbpassword.Location = new System.Drawing.Point(520, 289);
+            this.cbpassword.Name = "cbpassword";
+            this.cbpassword.Size = new System.Drawing.Size(125, 20);
+            this.cbpassword.TabIndex = 6;
+            this.cbpassword.Text = "Show Password";
+            this.cbpassword.UseVisualStyleBackColor = true;
+            this.cbpassword.CheckedChanged += new System.EventHandler(this.cbpassword_CheckedChanged);
+            // 
+            // pbadmin
+            // 
+            this.pbadmin.Image = ((System.Drawing.Image)(resources.GetObject("pbadmin.Image")));
+            this.pbadmin.Location = new System.Drawing.Point(33, 103);
+            this.pbadmin.Name = "pbadmin";
+            this.pbadmin.Size = new System.Drawing.Size(364, 332);
+            this.pbadmin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbadmin.TabIndex = 7;
+            this.pbadmin.TabStop = false;
+            // 
+            // AdminLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(231)))), ((int)(((byte)(235)))));
+            this.ClientSize = new System.Drawing.Size(857, 514);
+            this.Controls.Add(this.pbadmin);
+            this.Controls.Add(this.cbpassword);
+            this.Controls.Add(this.tbpassword);
+            this.Controls.Add(this.tbusername);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.lblpassword);
+            this.Controls.Add(this.lblusername);
+            this.Controls.Add(this.lbllogin);
+            this.Name = "AdminLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pbadmin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbllogin;
+        private System.Windows.Forms.Label lblusername;
+        private System.Windows.Forms.Label lblpassword;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox tbusername;
+        private System.Windows.Forms.TextBox tbpassword;
+        private System.Windows.Forms.CheckBox cbpassword;
+        private System.Windows.Forms.PictureBox pbadmin;
+    }
+}
---
+++ BeauteBliss/SalesReport.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace BeauteBliss
+{
+    public partial class SalesReport : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\BeauteBlissDB.mdf;Integrated Security=True");
+        public SalesReport()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadTotalSales()
+        {
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "SELECT ISNULL(SUM(Total),0) FROM Payment",
+                connection);
+
+            decimal totalSales = Convert.ToDecimal(cmd.ExecuteScalar());
+
+            lbltotalsales.Text = "RM " + totalSales.ToString("0.00");
+
+            connection.Close();
+        }
+
+        private void LoadTotalBookings()
+        {
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "SELECT COUNT(*) FROM Booking",
+                connection);
+
+            int totalBookings = Convert.ToInt32(cmd.ExecuteScalar());
+
+            lbltotalbookings.Text = totalBookings.ToString();
+
+            connection.Close();
+        }
+
+        private void LoadChart()
+        {
+            chartsales.Series.Clear();
+            chartsales.Titles.Clear();
+
+            Series series = new Series("Sales");
+            series.ChartType = SeriesChartType.Column;
+
+            chartsales.Series.Add(series);
+
+            SqlCommand cmd = new SqlCommand(
+                @"SELECT MONTH(PaymentDate) AS MonthNo,
+                  SUM(Total) AS TotalSales
+                  FROM Payment
+                  WHERE PaymentDate BETWEEN @startDate AND @endDate
+                  GROUP BY MONTH(PaymentDate)
+                  ORDER BY MonthNo",
+                connection);
+
+            cmd.Parameters.AddWithValue("@startDate", dtpstart.Value.Date);
+            cmd.Parameters.AddWithValue("@endDate", dtpend.Value.Date);
+
+            connection.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string monthName = new DateTime(
+                    DateTime.Now.Year,
+                    Convert.ToInt32(reader["MonthNo"]),
+                    1).ToString("MMM");
+
+                series.Points.AddXY(
+                    monthName,
+                    Convert.ToDecimal(reader["TotalSales"])
+                );
+            }
+
+            reader.Close();
+
+            chartsales.Titles.Add("Monthly Sales");
+
+            connection.Close();
+        }
+
+        private void LoadServiceCategoryChart()
+        {
+            chartcategory.Series.Clear();
+            chartcategory.Titles.Clear();
+
+            Series series = new Series("Service Category");
+            series.ChartType = SeriesChartType.Pie;
+
+            chartcategory.Series.Add(series);
+
+            SqlCommand cmd = new SqlCommand(
+                @"SELECT s.Category, COUNT(*) AS TotalBookings
+                  FROM Booking b
+                  INNER JOIN ServiceList s ON b.ServiceID = s.ServiceID
+                  WHERE b.BookingDate BETWEEN @startDate AND @endDate
+                  GROUP BY s.Category",
+                connection);
+
+            cmd.Parameters.AddWithValue("@startDate", dtpstart.Value.Date);
+            cmd.Parameters.AddWithValue("@endDate", dtpend.Value.Date);
+
+            connection.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                series.Points.AddXY(
+                    reader["Category"].ToString(),
+                    Convert.ToInt32(reader["TotalBookings"])
+                );
+            }
+
+            reader.Close();
+            connection.Close();
+
+            chartcategory.Titles.Add("Service Category Distribution");
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            AdminDashboard dashboard = new AdminDashboard();
+            dashboard.Show();
+            this.Hide();
+        }
+
+        private void btngenerate_Click(object sender, EventArgs e)
+        {
+            LoadTotalSales();
+            LoadTotalBookings();
+            LoadChart();
+            LoadServiceCategoryChart();
+
+            MessageBox.Show("Report Generated Successfully!");
+        }
+    }
+}

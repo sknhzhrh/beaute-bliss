64226ef5f247d450f8ad900690c62437bfa1f976
wan nursakinah <sakinahzahrah166@gmail.com>
2026-06-22 04:53:44 +0800

    Clean main and remove member modules

--- BeauteBliss/SQLQuery3.sql
+++
@@ -1,9 +0,0 @@
-﻿INSERT INTO Products (ProductName, Brand, Price, Quantity)
-VALUES
-('Facial Cleanser', 'Cetaphil', 35, 20),
-
-('Hair Serum', 'Mise En Scene', 50, 15),
-
-('Body Lotion', 'Vaseline', 25, 30),
-
-('Nail Polish', 'Innisfree', 20, 25);

1d5e9b842f853071934a464c358e5b3122ddccb7
NURULALLISSYA <nurulallissya@gmail.com>
2026-06-22 03:09:16 +0800

    Add customer module

---
+++ BeauteBliss/CustomerManagement.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BeauteBliss
+{
+    public partial class CustomerManagement : Form
+    {
+        SqlConnection connection = new SqlConnection(
+    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\BeauteBlissDB.mdf;Integrated Security=True");
+
+        int selectedCustomerID = 0;
+        public CustomerManagement()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void LoadCustomerData()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Customer", connection);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void CustomerManagement_Load(object sender, EventArgs e)
+        {
+            LoadCustomerData();
+        }
+
+        private void searchbtn_Click(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == "")
+            {
+                MessageBox.Show("Please enter Customer ID");
+                return;
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(
+                "SELECT * FROM Customer WHERE CustomerID=@CustomerID", connection);
+
+            adapter.SelectCommand.Parameters.AddWithValue("@CustomerID", txtSearch.Text);
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void editbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Customer", connection);
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+                DataTable dt = (DataTable)dataGridView1.DataSource;
+                adapter.Update(dt);
+
+                connection.Close();
+
+                MessageBox.Show("Customer Updated Successfully");
+                LoadCustomerData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                connection.Close();
+            }
+        }
+
+        private void delbtn_Click(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == "")
+            {
+                MessageBox.Show("Please enter Customer ID");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to delete this customer?",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM Customer WHERE CustomerID=@CustomerID", connection);
+
+                cmd.Parameters.AddWithValue("@CustomerID", txtSearch.Text);
+
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+
+                MessageBox.Show("Customer Deleted Successfully");
+
+                txtSearch.Clear();
+                LoadCustomerData();
+            }
+        }
+
+        private void refbtn_Click(object sender, EventArgs e)
+        {
+            selectedCustomerID = 0;
+            txtSearch.Clear();
+            LoadCustomerData();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                selectedCustomerID = Convert.ToInt32(row.Cells["CustomerID"].Value);
+                txtSearch.Text = selectedCustomerID.ToString();
+            }
+        }
+    }
+}
---
+++ BeauteBliss/Register.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace BeauteBliss
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.registerbtn = new System.Windows.Forms.Button();
+            this.txtPhoneNo = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.linklogin = new System.Windows.Forms.LinkLabel();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(69)))), ((int)(((byte)(96)))));
+            this.label3.Location = new System.Drawing.Point(428, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(242, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Create Your Account";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 403);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtName.Location = new System.Drawing.Point(492, 73);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(237, 25);
+            this.txtName.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(359, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Phone No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(359, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Name";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.Location = new System.Drawing.Point(492, 149);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(237, 25);
+            this.txtEmail.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(359, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(359, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(360, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 16);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Password";
+            // 
+            // registerbtn
+            // 
+            this.registerbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(69)))), ((int)(((byte)(96)))));
+            this.registerbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerbtn.ForeColor = System.Drawing.Color.White;
+            this.registerbtn.Location = new System.Drawing.Point(492, 348);
+            this.registerbtn.Name = "registerbtn";
+            this.registerbtn.Size = new System.Drawing.Size(237, 31);
+            this.registerbtn.TabIndex = 27;
+            this.registerbtn.Text = "REGISTER";
+            this.registerbtn.UseVisualStyleBackColor = false;
+            this.registerbtn.Click += new System.EventHandler(this.registerbtn_Click);
+            // 
+            // txtPhoneNo
+            // 
+            this.txtPhoneNo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPhoneNo.Location = new System.Drawing.Point(492, 110);
+            this.txtPhoneNo.Multiline = true;
+            this.txtPhoneNo.Name = "txtPhoneNo";
+            this.txtPhoneNo.Size = new System.Drawing.Size(237, 25);
+            this.txtPhoneNo.TabIndex = 28;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAddress.Location = new System.Drawing.Point(492, 186);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(237, 25);
+            this.txtAddress.TabIndex = 29;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Location = new System.Drawing.Point(492, 224);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(237, 25);
+            this.txtPassword.TabIndex = 30;
+            // 
+            // linklogin
+            // 
+            this.linklogin.ActiveLinkColor = System.Drawing.Color.Black;
+            this.linklogin.AutoSize = true;
+            this.linklogin.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linklogin.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(69)))), ((int)(((byte)(96)))));
+            this.linklogin.Location = new System.Drawing.Point(651, 399);
+            this.linklogin.Name = "linklogin";
+            this.linklogin.Size = new System.Drawing.Size(76, 16);
+            this.linklogin.TabIndex = 32;
+            this.linklogin.TabStop = true;
+            this.linklogin.Text = "Login here";
+            this.linklogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linklogin_LinkClicked);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(472, 399);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(173, 16);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Already have an account?";
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(231)))), ((int)(((byte)(235)))));
+            this.ClientSize = new System.Drawing.Size(855, 430);
+            this.Controls.Add(this.linklogin);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtPhoneNo);
+            this.Controls.Add(this.registerbtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Name = "Register";
+            this.Text = "Register";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button registerbtn;
+        private System.Windows.Forms.TextBox txtPhoneNo;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.LinkLabel linklogin;
+        private System.Windows.Forms.Label label9;
+    }
+}

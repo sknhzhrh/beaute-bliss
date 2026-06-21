8f34a79acdc3824a434a5f216a6a9abf4741784b
mrfthz <nurmihaa23@gmail.com>
2026-06-22 01:33:12 +0800

    customer page

---
+++ BeauteBliss/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+
+namespace BeauteBliss
+{
+    class DBConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;
+          AttachDbFilename=|DataDirectory|\BeauteblissDB.mdf;
+          Integrated Security=True");
+        }
+    }
+}
--- BeauteBliss/ReceiptForm.cs
+++ BeauteBliss/ReceiptForm.cs
@@ -16,5 +16,64 @@ namespace BeauteBliss
         {
             InitializeComponent();
         }
+
+        private void ReceiptForm_Load(object sender, EventArgs e)
+        {
+            dgvReceipt.Rows.Clear();
+
+            decimal grandTotal = 0;
+
+            foreach (CartItem item in CartManager.Cart)
+            {
+                decimal total =
+                    item.Price *
+                    item.Quantity;
+
+                dgvReceipt.Rows.Add(
+                    item.ItemName,
+                    item.Type,
+                    item.Quantity,
+                    item.Price,
+                    total
+                );
+
+                grandTotal += total;
+            }
+
+            decimal tax =
+                grandTotal * 0.06m;
+
+            grandTotal += tax;
+
+            lblGrandTotal.Text =
+                "RM " +
+                grandTotal.ToString("0.00");
+
+            lblDate.Text =
+                DateTime.Now.ToString(
+                    "dd/MM/yyyy hh:mm tt");
+
+            lblOrderNo.Text =
+                "ORD" +
+                DateTime.Now.ToString(
+                    "yyyyMMddHHmmss");
+        }
+
+        private void btnBackHome_Click(object sender, EventArgs e)
+        {
+            CartManager.Cart.Clear();
+
+            ServicesForm service =
+                new ServicesForm();
+
+            service.Show();
+
+            this.Close();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Receipt printed successfully!");
+        }
     }
 }
